cdb02fdba57ea1c17389cf469883eca1e6b47f59
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2025-03-16 15:14:07 +0100

    initial commit

---
+++ src/main/java/com/syed/exchange_rate_service/ExchangeRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syed.exchange_rate_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syed/exchange_rate_service/ExchangeRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syed.exchange_rate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
